8c716098378e732776a5cc0740907d61438375bf
jsxcii <jsxcii@gmail.com>
2015-10-28 09:08:45 +0100

    expand for MVC (MVVC)

---
+++ logic/controller.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+    Created by SCARL
+
+    Table of content
+    line 015-000: Initialization
+        line 000-000:
+        line 000-000:
+        line 000-000:
+        line 000-000:
+*/
+
+//  Initialization
