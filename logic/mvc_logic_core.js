a7a5ffea2fd956e6a3622590fbbea35b2e67ae25
jsxcii <jsxcii@gmail.com>
2015-12-15 15:41:48 +0100

    add jslint commands + change html string structure +  shorten the cleanNode function

--- logic/mvc_logic_core.js
+++ logic/mvc_logic_core.js
@@ -1,3 +1,4 @@
+/*jslint devel: true, passfail: false, plusplus: true, sloppy: true, maxerr: 400*/
 "use strict";
 /*
     Created by SCARL
@@ -39,111 +40,114 @@
     html_1stPage_Starter = document.getElementById("startlogo");
 
     // This will be the first menu to reach after the start screen
-    html_2ndPage_Menu = '\
-<header>\
-<img id="menuscarl" src="images/menuscarl.png" alt="SCARL @Player"  />\
-</header>\
-<hr>\
-<nav>\
-<ul>\
-<li id="vidPath">\
-<p>Videothek</p>\
-</li>\
-<li id="audPath">\
-<p>Audiothek</p>\
-</li>\
-<li id="impPath">\
-<p>Impressum</p>\
-</li>\
-</ul>\
-</nav>\
-<hr>\
-<br>\
-<img id="facebook" src="images/fb.png" alt="facebook"/>\
-<img id="twitter" src="images/twitter.png" alt="twitter"/>\
-<img id="instagram" src="images/instagram.png" alt="instagram"/>\
-<img id="tumblr" src="images/tumblr.png" alt="tumblr"/>\
-<footer>\
-<p id="datPath">Datenschutz</p>\
-</footer>';
+    html_2ndPage_Menu = '' +
+'<header>' +
+    '<img id="menuscarl" src="images/menuscarl.png" alt="SCARL @Player"  />' +
+'</header>' +
+'<hr>' +
+'<nav>' +
+    '<ul>' +
+        '<li id="vidPath">' +
+            '<p>Videothek</p>' +
+        '</li>' +
+        '<li id="audPath">' +
+            '<p>Audiothek</p>' +
+        '</li>' +
+        '<li id="impPath">' +
+            '<p>Impressum</p>' +
+        '</li>' +
+    '</ul>' +
+'</nav>' +
+'<hr>' +
+'<br>' +
+'<img id="facebook" src="images/fb.png" alt="facebook"/>' +
+'<img id="twitter" src="images/twitter.png" alt="twitter"/>' +
+'<img id="instagram" src="images/instagram.png" alt="instagram"/>' +
+'<img id="tumblr" src="images/tumblr.png" alt="tumblr"/>' +
+'<footer>' +
+'<p id="datPath">Datenschutz</p>' +
+'</footer>';
 
-    // whole impressum site
-    html_3rdPage_Impressum = '\
-<div class="naviii">\
-<ul>\
-<li>\
-<span>@Scarl Startseite</span>\
-<img id="logo" src="images/scarl.png" alt="SCARL @Player"/>\
-</li>\
-<li>\
-<span>@Scarl Suche</span>\
-<img id="suche" src="images/suche.png" alt="suche"/>\
-</li>\
-</ul>\
-</div>\
-<div class="inhalt">\
-<h1>Impressum</h1>\
-<p>Angaben gemäß § 5 TMG</p>\
-<br><p>Beuth-Hochschule für Technik <br>\
-Luxemburger Straße 10<br>\
-13353 Berlin <br>\
-<br>\
-</p>\
-<p><strong>Vertreten durch: </strong><br>\
-Beuth-Hochschule für Technik<br><br></p>\
-<p><strong>Kontakt:</strong> <br>\
-Telefon: 0000000-0000000<br>\
-E-Mail: <a href="mailto:aaa@live.de">aaa@live.de</a></br><br></p>\
-<p><strong>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</strong><br>\
-Beuth-Hochschule für Technik <br>\
-Luxemburger Straße 10<br>\
-13353 Berlin <br><br></p>\
-<p><strong>Haftungsausschluss: </strong><br>\
-<br><strong>Haftung für Inhalte</strong><br><br>\
-Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt.\
-Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können\
-wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG\
-für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich.\
-Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte\
-oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen,\
-die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung\
-oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen\
-bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem\
-Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich.\
-Bei Bekanntwerden von entsprechenden Rechtsverletzungen\
-werden wir diese Inhalte umgehend entfernen.\
-<br><br><strong>Urheberrecht</strong><br><br>\
-Die durch die Seitenbetreiber erstellten Inhalte\
-und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht.\
-Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art\
-der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen\
-der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.\
-Downloads und Kopien dieser Seite sind nur für den privaten,\
-nicht kommerziellen Gebrauch gestattet.\
-Soweit die Inhalte auf dieser Seite nicht vom Betreiber\
-erstellt wurden, werden die Urheberrechte Dritter beachtet.\
-Insbesondere werden Inhalte Dritter als solche gekennzeichnet.\
-Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden,\
-bitten wir um einen entsprechenden Hinweis.\
-Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte\
-umgehend entfernen.\
-<br><br><strong>Datenschutz</strong><br><br>\
-Die Nutzung unserer Webseite ist in der Regel ohne Angabe\
-personenbezogener Daten möglich.\
-Soweit auf unseren Seiten personenbezogene Daten\
-(beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden,\
-erfolgt dies, soweit möglich, stets auf freiwilliger Basis.\
-Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.\
-<br> Wir weisen darauf hin, dass die Datenübertragung im Internet\
-(z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann.\
-Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.\
-<br> Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten\
-Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich\
-angeforderter Werbung und Informationsmaterialien wird hiermit\
-ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich\
-ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von\
-Werbeinformationen, etwa durch Spam-Mails, vor.\
-<br></p><br></div>';
+    html_3rdPage_Impressum = '' +
+'<div class="naviii">' +
+    '<ul>' +
+        '<li>' +
+            '<span>@Scarl Startseite</span>' +
+            '<img id="logo" src="images/scarl.png" alt="SCARL @Player"/>' +
+        '</li>' +
+        '<li>' +
+            '<span>@Scarl Suche</span>' +
+            '<img id="suche" src="images/suche.png" alt="suche"/>' +
+        '</li>' +
+    '</ul>' +
+'</div>' +
+'<div class="inhalt">' +
+    '<h1>Impressum</h1>' +
+    '<p>Angaben gemäß § 5 TMG</p>' +
+    '<br>' +
+    '<p>Beuth-Hochschule für Technik ' +
+        '<br>' +
+        'Luxemburger Straße 10' +
+        '<br>' +
+        '13353 Berlin' +
+        '<br>' +
+        '<br>' +
+    '</p>' +
+    '<p>' +
+        '<strong>Vertreten durch: </strong>' +
+        '<br>' +
+        'Beuth-Hochschule für Technik' +
+        '<br>' +
+        '<br>' +
+    '</p>' +
+    '<p>' +
+        '<strong>Kontakt:</strong>' +
+        '<br>' +
+        'Telefon: 0000000-0000000' +
+        '<br>' +
+        'E-Mail: <a href="mailto:aaa@live.de">aaa@live.de</a>' +
+        '<br>' +
+        '<br>' +
+    '</p>' +
+    '<p>' +
+        '<strong>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</strong>' +
+        '<br>' +
+        'Beuth-Hochschule für Technik' +
+        '<br>' +
+        'Luxemburger Straße 10' +
+        '<br>' +
+        '13353 Berlin' +
+        '<br>' +
+        '<br>' +
+    '</p>' +
+    '<p>' +
+        '<strong>Haftungsausschluss: </strong>' +
+        '<br>' +
+        '<br>' +
+        '<strong>Haftung für Inhalte</strong>' +
+        '<br>' +
+        '<br>' +
+'Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.' +
+        '<br>' +
+        '<br>' +
+        '<strong>Urheberrecht</strong>' +
+        '<br>' +
+        '<br>' +
+'Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.' +
+        '<br>' +
+        '<br>' +
+        '<strong>Datenschutz</strong>' +
+        '<br>' +
+        '<br>' +
+'Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.' +
+        '<br>' +
+'Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann.Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.' +
+        '<br>' +
+'Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.' +
+        '<br>' +
+    '</p>' +
+    '<br>' +
+'</div>';
 
 
     /************************************************************
@@ -153,8 +157,7 @@ Werbeinformationen, etwa durch Spam-Mails, vor.\
     function cleanNode(node) {
 
 
-        var node = node,
-            nodeChild;
+        var nodeChild;
 
         if (node.firstChild) {
 
@@ -171,7 +174,7 @@ Werbeinformationen, etwa durch Spam-Mails, vor.\
 
     }
 
-
+/*************************************************************
     function stackCleaner(caller, handlerType, handlerName) {
 
 
@@ -185,7 +188,7 @@ Werbeinformationen, etwa durch Spam-Mails, vor.\
 
 
     }
-
+****************************************************************/
 
     function requestMaker(targetPHP) {
 
@@ -233,6 +236,8 @@ Werbeinformationen, etwa durch Spam-Mails, vor.\
             tempNodeList,
             tempNodeListLength,
             child;
+
+        cleanNode(parent);
 
         temp = document.createElement('div');
 
@@ -299,10 +304,22 @@ Werbeinformationen, etwa durch Spam-Mails, vor.\
     }
 
 
-    function addEvent(caller, eventType, handlerName) {
+    function addEvent(caller, eventType, handlerFunction) {
 
 
-        caller.addEventListener(eventType, handlerName);
+        caller.addEventListener(eventType, function handler() {
+
+
+            var caller = this;
+
+            handlerFunction();
+
+            caller.removeEventListener(eventType, handler);
+
+
+        });
+
+        caller = null;
 
 
     }
@@ -316,16 +333,15 @@ Werbeinformationen, etwa durch Spam-Mails, vor.\
     function initialize3rdPageImpressum() {
 
 
-        // do init impressum
         var logo = document.getElementById("logo"),
             search = document.getElementById("suche");
 
-        console.log("logo: " + logo);
 
         addEvent(logo, "click", moveTo2ndPage);
 
 
     }
+
 
     function initialize3rdPageVideoLibrary() {
 
@@ -353,16 +369,13 @@ Werbeinformationen, etwa durch Spam-Mails, vor.\
 
     }
 
+
     function moveTo3rdPageImpressum() {
 
-
-        cleanNode(bodyTagElement);
 
         constructHTMLatParent(bodyTagElement, html_3rdPage_Impressum);
 
         initialize3rdPageImpressum();
-
-        stackCleaner(this, 'click', moveTo3rdPageImpressum);
 
 
     }
@@ -402,13 +415,9 @@ Werbeinformationen, etwa durch Spam-Mails, vor.\
     function moveTo2ndPage() {
 
 
-        cleanNode(bodyTagElement);
-
         constructHTMLatParent(bodyTagElement, html_2ndPage_Menu);
 
         initialize2ndPage();
-
-        stackCleaner(this, 'click', moveTo2ndPage);
 
 
     }
